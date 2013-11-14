5a2b8ae859d7572e68e11b5734c0f50f5c199903
Jaime Pillora <jpillora@gmail>
2013-11-14 16:42:21 +1100

    pushing to heroku

--- balancer.js
+++ balancer.js
@@ -1,20 +1,27 @@
 var bouncy = require('bouncy');
 
-var APP_NAME = process.env.APP_NAME;
+var APP_NAME = process.env.APP_NAME || 'node-load-tester';
 var BASE_DOMAIN = process.env.BASE_DOMAIN || '.herokuapp.com';
-var NUM_INSTANCES = parseInt(process.env.INSTANCES,10) || 0;
+var NUM_INSTANCES = parseInt(process.env.INSTANCES,10) || 3;
 
 // DEPLOY DIR 'git@heroku.com:NAME-INSTANCEID.git'
 // var AUTH_KEY = new Buffer(":"+process.env.API_KEY).toString('base64');
 
-var curr = 0;
+var curr = 1;
 
 var server = bouncy(function (req, res, bounce) {
   var hostname = APP_NAME + "-" + curr + BASE_DOMAIN;
-  console.log('Bounce to #%s: %s', curr, host);
-  curr = (curr + 1) % NUM_INSTANCES;
-  req.headers.Host = hostname;
-  bounce(hostname, 80);
+  console.log('Bounce to #%s: %s', curr, hostname);
+
+  bounce(hostname, 80, {
+    headers: {
+      host: hostname
+    }
+  });
+
+  //increment
+  curr++;
+  if(curr > NUM_INSTANCES) curr = 1;
 });
 
 var port = process.env.PORT || 3000;
